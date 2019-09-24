78a132993e396d3d6c0197c389bc26172409af46
August Oppenheimer <gloop@Augusts-MacBook-Pro.local>
2019-09-24 08:12:05 -0500

    Remove silly console logs

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('woowoo')
-
 const weatherForm = document.querySelector('form')
 
 weatherForm.addEventListener('submit', (event) => {
@@ -7,9 +5,7 @@ weatherForm.addEventListener('submit', (event) => {
     const location = document.querySelector('.location-select').value
     document.querySelector('#location-name').textContent = 'Loading...';
     fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
-        console.log(response)
         response.json().then((data) => {
-            console.log(data)
             document.querySelector('#location-name').textContent = data.location;
             document.querySelector('#location-forecast').textContent = data.forecast;
         })
