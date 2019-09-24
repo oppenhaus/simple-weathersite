5478f7843cb7e93642b7e9ad48d9486eadaf21b1
August Oppenheimer <gloop@Augusts-MacBook-Pro.local>
2019-09-24 08:43:41 -0500

    Connecting Heroku Deployment stuff

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@ weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
     const location = document.querySelector('.location-select').value
     document.querySelector('#location-name').textContent = 'Loading...';
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             document.querySelector('#location-name').textContent = data.location;
             document.querySelector('#location-forecast').textContent = data.forecast;
